ab7fac75ec8247deafa15ab2a6f2a7e9f4a8f945
Danillo Neto <danilloneto98@gmail.com>
2026-01-21 16:58:16 -0300

    CHORE: Update pubspec.lock e plugins gerados (pós-pub get)

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,8 +5,10 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   file_selector_windows
+  firebase_core
   flutter_secure_storage_windows
   url_launcher_windows
+  windows_single_instance
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
